b3e5bcb2e029dc6db4bd2c68a08a95626c32b5ff
ananuness <beatriznunez601@gmail.com>
2024-05-14 23:14:59 -0300

    init: initial setup

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    bail: 1,
+    update: true,
+    coverage: {
+      // include: ['./src/**'],
+      enabled: true,
+    },
+  },
+});
