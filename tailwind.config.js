1c40d4ec3b575905779c5d9f864a7595c72b41e1
ananuness <beatriznunez601@gmail.com>
2024-05-16 16:19:07 -0300

    chore: updating tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       fontFamily: {
         sans: ['Nunito', 'sans-serif'],
       },
+      boxShadow: {
+        sm: '0 2px 10px 0 rgba(0, 0, 0, .10)',
+        lg: '0 0 64px 0 rgba(0, 0, 0, .25)',
+      },
       colors: {
         blue: {
           900: '#02274F',
@@ -14,8 +18,16 @@ export default {
           400: '#FDCF00',
         },
         zinc: {
+          0: '#FDFDFD',
+          50: '#FAFAFA',
           100: '#F4F4F4',
           500: '#999999',
+        },
+        slate: {
+          100: '#F1F5F9',
+        },
+        neutral: {
+          800: '#262626',
         },
       },
     },
