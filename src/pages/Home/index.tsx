c8b4725e4d3c17b6a16ca29cc26e2825b25fa9ca
ananuness <beatriznunez601@gmail.com>
2024-05-17 08:07:13 -0300

    feat: creating the home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,27 @@
+import { useAuth } from '@/hooks/useAuth';
+import { useProfileDetails } from '@/hooks/useProfileDetais';
+import { UserCard } from '@/components/organisms/UserCard';
+import { Button } from '@/components/atoms/Button';
+
+export function Home() {
+  const { signOut } = useAuth();
+  const { userProfile } = useProfileDetails();
+
+  return (
+    userProfile && (
+      <div className="min-h-screen bg-slate-100">
+        <header className="flex justify-end px-8 py-3 bg-white">
+          <Button title="Logout" onClick={signOut} />
+        </header>
+        <main className="flex justify-center mt-24 px-8 pb-8">
+          <UserCard
+            name={userProfile!.name}
+            lastName={userProfile?.last_name}
+            email={userProfile!.email}
+            avatar={userProfile?.avatar}
+          />
+        </main>
+      </div>
+    )
+  );
+}
