ce5da157c7f54761d0ef7ab56a45dc238cd7fa06
ananuness <beatriznunez601@gmail.com>
2024-05-19 16:41:33 -0300

    feat: updating Home page to use loading state

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,29 @@
 import { useAuth } from '@/hooks/useAuth';
-import { useProfileDetails } from '@/hooks/useProfileDetais';
+import { useProfileDetails } from '@/hooks/useProfileDetails';
 import { UserCard } from '@/components/organisms/UserCard';
 import { Button } from '@/components/atoms/Button';
 
 export function Home() {
   const { signOut } = useAuth();
-  const { userProfile } = useProfileDetails();
+  const { loading, userProfile } = useProfileDetails();
 
   return (
-    userProfile && (
-      <div className="min-h-screen bg-slate-100">
-        <header className="flex justify-end px-8 py-3 bg-white">
-          <Button title="Logout" onClick={signOut} />
-        </header>
-        <main className="flex justify-center mt-24 px-8 pb-8">
-          <UserCard
-            name={userProfile!.name}
-            lastName={userProfile?.last_name}
-            email={userProfile!.email}
-            avatar={userProfile?.avatar}
-          />
-        </main>
-      </div>
-    )
+    <div className="min-h-screen bg-slate-100">
+      {!loading && userProfile && (
+        <>
+          <header className="flex justify-end px-8 py-3 bg-white">
+            <Button title="Logout" onClick={signOut} />
+          </header>
+          <main className="flex justify-center mt-24 px-8 pb-8">
+            <UserCard
+              name={userProfile!.name}
+              lastName={userProfile?.last_name}
+              email={userProfile!.email}
+              avatar={userProfile?.avatar}
+            />
+          </main>
+        </>
+      )}
+    </div>
   );
 }
